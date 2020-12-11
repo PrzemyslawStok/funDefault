786fb87cdfeccd1775bc07a1c900c50ebc63c440
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-11 17:37:20 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,20 +5,34 @@ fun main() {
     val b = 5.0
     val c = 1.0
 
-    squareEquation(a,b,c)
+    equation(a,b,c)
+    equation(1.0,5.0,8.0)
 }
 
-fun squareEquation(a: Double, b: Double, c:Double){
+fun equation(a: Double, b: Double, c:Double){
+
+    if(a!=0.0&&b!=0.0&&c!=0.0){
+        //ax^2+bx+c = 0
+        println("Obliczam rowziązania równania kwadratowego: ${a}x^2 + ${b}x + $c = 0")
+    }
+
+    if(a==0.0&&b!=0.0&&c!=0.0){
+        //bx+c = 0
+        println("Obliczam rowziązania równania liniowego: ${b}x + $c = 0")
+    }
+
     val delta = deltaFun(a,b,c)
 
     println("delta wynosi: $delta")
 
     if(delta>0){
+        println("Równanie posiada dwa rozwiązania: ")
         println("x1 = ${x1(a,b,delta)}")
         println("x2 = ${x2(a,b,delta)}")
     }
 
     if(delta==0.0){
+        println("Równanie posiada jedno rozwiązanie: ")
         //x = -b/2*a
     }
 }
