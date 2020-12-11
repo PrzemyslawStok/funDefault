6f14bda6ea637acc0c1a811f5fa6d99974cb571d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-11 17:42:50 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,40 +1,39 @@
 import kotlin.math.sqrt
 
 fun main() {
-    val a = 1.0
-    val b = 5.0
-    val c = 1.0
 
-    equation(a,b,c)
-    equation(1.0,5.0,8.0)
+    equation(1.0,5.0,1.0)
+    equation(2.0,1.0)
 }
 
-fun equation(a: Double, b: Double, c:Double){
+fun equation(c: Double, b: Double, a:Double = 0.0){
 
     if(a!=0.0&&b!=0.0&&c!=0.0){
         //ax^2+bx+c = 0
-        println("Obliczam rowziązania równania kwadratowego: ${a}x^2 + ${b}x + $c = 0")
+        println("Obliczam rozwiązania równania kwadratowego: ${a}x^2 + ${b}x + $c = 0")
+
+        val delta = deltaFun(a,b,c)
+
+        println("delta wynosi: $delta")
+
+        if(delta>0){
+            println("Równanie posiada dwa rozwiązania: ")
+            println("x1 = ${x1(a,b,delta)}")
+            println("x2 = ${x2(a,b,delta)}")
+        }
+
+        if(delta==0.0){
+            println("Równanie posiada jedno rozwiązanie: ")
+            //x = -b/2*a
+        }
     }
 
     if(a==0.0&&b!=0.0&&c!=0.0){
         //bx+c = 0
-        println("Obliczam rowziązania równania liniowego: ${b}x + $c = 0")
+        println("Obliczam rozwiązania równania liniowego: ${b}x + $c = 0")
+        val x = -c/b
     }
 
-    val delta = deltaFun(a,b,c)
-
-    println("delta wynosi: $delta")
-
-    if(delta>0){
-        println("Równanie posiada dwa rozwiązania: ")
-        println("x1 = ${x1(a,b,delta)}")
-        println("x2 = ${x2(a,b,delta)}")
-    }
-
-    if(delta==0.0){
-        println("Równanie posiada jedno rozwiązanie: ")
-        //x = -b/2*a
-    }
 }
 
 fun deltaFun(a: Double, b: Double, c:Double):Double{
